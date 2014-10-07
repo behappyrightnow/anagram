0e6594fb99802d58f9bae37902155a308fc7ce53
Somik Raha <somikr@gmail.com>
2014-10-07 14:06:06 -0700

    step5 - one to many mappings done

--- test/anagramSpec.js
+++ test/anagramSpec.js
@@ -23,14 +23,29 @@ describe('Anagram tests', function () {
     var anagram = new Anagram(["apple"]);
     expect(anagram.wordList).toEqual(["apple"]);
     });*/
-    it("should make dictionary with keys", function () {
-        var anagram = new Anagram(["apple", "simple"]);
-        var keys = Object.keys(anagram.wordDict);
-        expect(keys.length).toBe(2);
-        var expectedKeys = ["aelpp", "eilmps"];
-        for (var i = 0; i < expectedKeys.length; i++) {
-            expect(keys.indexOf(expectedKeys[i]) !== -1).toBe(true);
-        }
+    describe("should make dictionary with keys", function () {
+        it("for one to one mappings", function () {
+            var anagram = new Anagram(["apple", "simple"]);
+            var keys = Object.keys(anagram.wordDict);
+            expect(keys.length).toBe(2);
+            var expectedKeys = ["aelpp", "eilmps"];
+            for (var i = 0; i < expectedKeys.length; i++) {
+                expect(keys.indexOf(expectedKeys[i]) !== -1).toBe(true);
+            }
+        });
+        it("for one to many mappings", function () {
+            var anagram = new Anagram(["odor", "door", "easel", "lease"]);
+            var keys = Object.keys(anagram.wordDict);
+            expect(keys.length).toBe(2);
+            var expectedKeys = ["door", "aeels"];
+            for (var i = 0; i < expectedKeys.length; i++) {
+                expect(keys.indexOf(expectedKeys[i]) !== -1).toBe(true);
+            }
+            var doorWords = ["odor", "door"];
+            var aeelsWords = ["easel", "lease"];
+            expect(anagram.wordDict["door"]).toEqual(doorWords);
+            expect(anagram.wordDict["aeels"]).toEqual(aeelsWords);
+        });
     });
 });
 //# sourceMappingURL=anagramSpec.js.map
